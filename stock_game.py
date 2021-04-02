9650879e5d9c48ea662ae966f8b71c0188cea08a
Floyd Hightower <floyd.hightower27@gmail.com>
2021-04-02 12:46:31 -0400

    Improving game to rely more heavily on columbo

--- stock_game.py
+++ stock_game.py
@@ -10,35 +10,42 @@ STOCKS = (('ford', 5, 13), ('apple', 100, 212))
 STARTING_MONEY = 100
 
 
-def _calculate_buy_options(answers: columbo.Answers):
-    if answers.get('stock_in_portfolio'):
-        return [str(i) for i in range(1, answers['stock_in_portfolio'] + 1)]
-    else:
-        max_quantity = math.floor(answers['money']/answers['stock_price'])
+def _get_stock_name_options(answers: columbo.Answers):
+    if answers['action'] == 'sell':
+        return [stock[0] for stock in STOCKS]
+    elif answers['action'] == 'buy':
+        return [stock[0] for stock in STOCKS if answers['prices'][stock[0]] <= answers['money']]
+
+
+def _get_stock_quantity_options(answers: columbo.Answers):
+    if answers['action'] == 'sell':
+        return [str(i) for i in range(1, answers['portfolio'][answers['stock_name']] + 1)]
+    elif answers['action'] == 'buy':
+        max_quantity = math.floor(answers['money'] / answers['prices'][answers['stock_name']])
         return [str(i) for i in range(1, max_quantity + 1)]
 
 
 questions = [
     columbo.Choice(
-        "action",
-        "What would you like to do?",
-        default="buy",
-        options=["buy", "sell", "view", "done"]
+        'action',
+        'What would you like to do?',
+        default='buy',
+        options=['buy', 'sell', 'view', 'done'],
     ),
     columbo.Choice(
-        "stock_name",
-        "Which stock?",
+        'stock_name',
+        'Which stock?',
         default=STOCKS[0][0],
-        options=lambda answers: [stock[0] for stock in STOCKS if answers['prices'][stock[0]] <= answers['money']],
+        options=_get_stock_name_options,
         should_ask=lambda answers: answers['action'] == 'buy' or answers['action'] == 'sell',
     ),
     columbo.Choice(
-        "stock_quantity",
-        "How much?",
+        'how_much',
+        'How much?',
         default='1',
-        options=_calculate_buy_options,
+        options=_get_stock_quantity_options,
         should_ask=lambda answers: answers['action'] == 'buy' or answers['action'] == 'sell',
-    )
+    ),
 ]
 
 
@@ -51,45 +58,52 @@ def _is_last_day(day):
     return NUMBER_OF_DAYS - day == 1
 
 
-def buy(money, stock_portfolio, prices):
+def buy(money, stock_portfolio, prices, answers: columbo.Answers):
     stock_price = prices[answers['stock_name']]
-    cost = stock_price * answers['stock_quantity']
+    stock_quantity = int(answers['how_much'])
+    cost = stock_price * stock_quantity
 
     money -= cost
-    stock_portfolio[stock_name] += stock_quantity
+    stock_portfolio[answers['stock_name']] += stock_quantity
 
     return money, stock_portfolio
 
 
 def sell(money, stock_portfolio, prices, answers: columbo.Answers):
     stock_price = prices[answers['stock_name']]
-    cost = stock_price * answers['stock_quantity']
+    stock_quantity = int(answers['how_much'])
+    cost = stock_price * stock_quantity
 
     money += cost
-    stock_portfolio[stock_name] -= stock_quantity
+    stock_portfolio[answers['stock_name']] -= stock_quantity
 
     return money, stock_portfolio
 
 
-def _print_output(money, stock_portfolio):
-    print(f'Money: {money}')
+def _print_output(money, stock_portfolio, prices):
+    print(f'\nMoney: {money}')
     print(f'Your portfolio: {stock_portfolio}')
+    print(f'Prices: {prices}\n')
 
 
 def _play_day(money, stock_portfolio):
     prices = _get_prices()
-    print(f'Prices: {prices}')
 
     while True:
-        _print_output(money, stock_portfolio)
-        answers = columbo.get_answers(questions, answers={'money': money, 'portfolio': stock_portfolio, 'prices': prices})
+        _print_output(money, stock_portfolio, prices)
+        try:
+            answers = columbo.get_answers(
+                questions,
+                answers={'money': money, 'portfolio': stock_portfolio, 'prices': prices},
+            )
+        except ValueError:  # a ValueError will be raised if the user wants to buy/sell stock he/she cannot
+            continue
 
         if answers['action'] == 'buy':
-            money, stock_portfolio = buy(money, stock_portfolio, prices)
+            money, stock_portfolio = buy(money, stock_portfolio, prices, answers)
         elif answers['action'] == 'sell':
             money, stock_portfolio = sell(money, stock_portfolio, prices, answers)
         elif answers['action'] == 'view':
-            # todo: handle this action
             pass
         else:
             # we're done!
@@ -112,7 +126,7 @@ def play():
         if _is_last_day(day):
             message = f'You\'re done! You ended up with ${money} (+{money - STARTING_MONEY})'
             print(message)
-    
+
         day += 1
 
 
